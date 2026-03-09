6e71b5757a896fc2d0bf651ad4ecf5f6a366bc2b
Mohit Verma <mohitsoni5842@gmail.com>
2026-03-09 12:37:52 +0530

    Update main.jsx

--- src/component/main.jsx
+++ src/component/main.jsx
@@ -5,9 +5,13 @@ import Skill from "./main/section3/skill.jsx";
 import Work from "./main/section4/Work.jsx";
 import Contact from "./main/section5/Contact.jsx";
 import Footer from "./main/section6/Footer.jsx";
+import bg from "../assets/bg.jpeg";
 const main = () => {
   return (
-    <div className="main w-full h-full bg-[url('src/component/main/bg.jpeg')] bg-cover bg-center">
+    <div
+      className="main w-full h-full bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
       <Header />
       <Intro />
       <Skill />
